4cf5ed86fde1770ad5c3351d8c86a4e7304992e9
NS-Sheam <123sheamfeni@gmail.com>
2024-01-09 21:21:47 +0600

    no change

--- src/pages/Dashboard/Chart.jsx
+++ src/pages/Dashboard/Chart.jsx
@@ -13,7 +13,7 @@ const Chart = () => {
                     font: {
                         size: 14,
                     },
-                    color: '#fff',
+                    // color: '#fff',
                 },
             },
         },
