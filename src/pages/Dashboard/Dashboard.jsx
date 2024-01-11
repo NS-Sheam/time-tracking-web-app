449d775c9ce7e5096de5d1aba4856b8314c6ece9
NS-Sheam <123sheamfeni@gmail.com>
2024-01-11 12:05:55 +0600

    dashboard modification

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,23 @@
 // Dashboard.js
 
+import { FaPlay } from "react-icons/fa";
+
 
 
 const Dashboard = () => {
 
     return (
         <div className="">
-            <>dfdfdf</>
+            <div>
+                <div className="font-bold">
+                    <h3 className="text-2xl">Today</h3>
+                    <p className="text-xl">Mon 22, 2023|10 AM</p>
+                </div>
+                <div>
+                    <span>Start Time Tracker</span>
+                    <span className="p-3 rounded-md bg-yellow-300 text-black"><FaPlay className="" /></span>
+                </div>
+            </div>
         </div>
     );
 };
