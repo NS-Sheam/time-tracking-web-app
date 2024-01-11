f195e17c982d63f572a96585f16bbb17bf3b546f
NS-Sheam <123sheamfeni@gmail.com>
2024-01-11 12:27:34 +0600

    some change

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,55 @@
-// Dashboard.js
+import weeklyactivity from "../../assets/icons/logo-1.png";
+import thisWeekIcon from "../../assets/icons/logo-2.png";
+import projectIcon from "../../assets/icons/folder-logo.png";
 
-import { FaPlay } from "react-icons/fa";
+import { FaEllipsisV, FaPlay } from "react-icons/fa";
 
 
 
 const Dashboard = () => {
 
     return (
-        <div className="">
+        <div className="space-y-3">
             <div className="flex items-center justify-between">
-                <div className="font-bold py-4">
-                    <h3 className="text-4xl">Today</h3>
-                    <p className="text-xl">Mon 22, 2023|10 AM</p>
+                <div className="py-4 space-y-1">
+                    <h3 className="font-bold text-4xl">Today</h3>
+                    <p className="text-xl font-semibold">Mon 22, 2023|10 AM</p>
                 </div>
-                <div className="flex items-center justify-center gap-4">
-                    <p>Start Time Tracker</p>
-                    <p className="p-3 rounded-md bg-yellow-300 text-black text-xl"><FaPlay className="" /></p>
+                <div className="flex items-center justify-center gap-4 bg-white rounded-md p-3 shadow-md">
+                    <p className="text-xl font-bold">Start Time Tracker</p>
+                    <p className="p-6 rounded-md bg-yellow-400 text-black text-xl"><FaPlay className="" /></p>
+                </div>
+            </div>
+            <div className="grid grid-cols-3 gap-3">
+                <div className="bg-white p-6 rounded-md space-y-2 shadow-md">
+                    <div className="flex items-center justify-between gap-2">
+                        <h4 className="font-bold">Weekly Activity</h4>
+                        <p><FaEllipsisV /></p>
+                    </div>
+                    <div className="flex items-center justify-between gap-2">
+                        <h3 className="text-3xl font-bold">0%</h3>
+                        <img className="bg-yellow-100 p-2 rounded-md w-14 h-14" src={weeklyactivity} alt="" />
+                    </div>
+                </div>
+                <div className="bg-white p-6 rounded-md space-y-2 shadow-md">
+                    <div className="flex items-center justify-between gap-2">
+                        <h4 className="font-bold">Worked This Week</h4>
+                        <p><FaEllipsisV /></p>
+                    </div>
+                    <div className="flex items-center justify-between gap-2">
+                        <h3 className="text-3xl font-bold">40:00:05</h3>
+                        <img className="bg-yellow-100 p-2 rounded-md w-14 h-14" src={thisWeekIcon} alt="" />
+                    </div>
+                </div>
+                <div className="bg-white p-6 rounded-md space-y-2 shadow-md">
+                    <div className="flex items-center justify-between gap-2">
+                        <h4 className="font-bold">Project Worked</h4>
+                        <p><FaEllipsisV /></p>
+                    </div>
+                    <div className="flex items-center justify-between gap-2">
+                        <h3 className="text-3xl font-bold">02</h3>
+                        <img className="bg-yellow-100 p-2 rounded-md w-14 h-14" src={projectIcon} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
