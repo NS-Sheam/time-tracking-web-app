a4485f04e9a762e350cbaf0f709f42650120c1f2
NS-Sheam <123sheamfeni@gmail.com>
2024-01-12 15:45:29 +0600

    unused item removed

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -1,19 +1,12 @@
-
-import { FaBars, FaHome, FaTasks, FaCog, FaChartBar, FaXing } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 import dashboardLogo from "../../../assets/icons/dashboard.png"
-import timesheetLogo from "../../../assets/icons/time-schedule.png"
 import analyticsLogo from "../../../assets/icons/analytics.png"
 import todoLogo from "../../../assets/icons/todo.png"
 import reportLogo from "../../../assets/icons/report.png"
 import settingLogo from "../../../assets/icons/setting.png"
 import './Navbar.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTitle } from '../../../redux/features/headerSlice';
 import ActiveLink from '../../ActiveLink';
 
-const Navbar = ({ drawerOpen, toggleDrawer }) => {
-    const dispatch = useDispatch();
+const Navbar = () => {
 
 
     const menuItems = [
@@ -24,7 +17,6 @@ const Navbar = ({ drawerOpen, toggleDrawer }) => {
         { icon: settingLogo, text: 'Settings', path: '/settings' },
     ];
 
-    const commonClasses = 'text-2xl icon';
 
     return (
         <div className={`fixed h-screen  text-white w-64 transition-all duration-300 py-2`}>
