08e0a3b605fc60847a3b5cc5f366c3a9a30cb75c
NS-Sheam <123sheamfeni@gmail.com>
2024-01-12 15:48:16 +0600

    little refector

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,7 @@
 import MainLayout from "../layout/MainLayout";
 import Dashboard from "../pages/Dashboard/Dashboard";
-import ProjectList from "../pages/ProjectList/ProjectList";
 import TimerView from "../pages/TimerView/TimerView";
 import Settings from "../pages/Settings/Settings";
-import Integrations from "../pages/Integrations/Integrations";
-
-import Accessibility from "../pages/Accessibility/Accessibility";
 import { createBrowserRouter } from "react-router-dom";
 import Todo from "../pages/Todo/Todo";
 import Report from "../pages/Report/Report";
@@ -17,12 +13,9 @@ const routes = createBrowserRouter([
     errorElement: <h1>Not Found</h1>,
     children: [
       { path: "/", element: <Dashboard /> },
-      { path: "/projects", element: <ProjectList /> },
       { path: "/time-tracking", element: <TimerView /> },
       { path: "/settings", element: <Settings /> },
-      { path: "/integrations", element: <Integrations /> },
       { path: "/todo", element: <Todo /> },
-      { path: "/accessibility", element: <Accessibility /> },
       { path: "/report", element: <Report /> },
     ],
   },
